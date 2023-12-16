ea8453e64f486d21f574ba300434c752756ecd4f
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2023-12-16 22:53:41 +0500

    error was fixed

--- location_app/models.py
+++ location_app/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-from user_app.models import CustomUser
+
 
 
 # Create your models here.
 class Location(models.Model):
-    # user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True)
     name = models.CharField(max_length=500)
 
     def __str__(self):
