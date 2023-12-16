6c819f349e47c4df508496faf3d0c73b669a6553
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2023-12-16 22:52:49 +0500

    error was fixed

--- location_app/models.py
+++ location_app/models.py
@@ -4,7 +4,7 @@ from user_app.models import CustomUser
 
 # Create your models here.
 class Location(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True)
+    # user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True)
     name = models.CharField(max_length=500)
 
     def __str__(self):
